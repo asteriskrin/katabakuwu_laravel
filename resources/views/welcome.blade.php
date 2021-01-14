3aeb6bbdf57bb8ef526913d74d2f30912a4f77e7
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-01-14 14:35:26 +0700

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,97 +4,175 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Katabakuwu</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
         <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <title>Katabakuwu</title>
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
+            * {
                 margin: 0;
+                padding: 0;
             }
 
-            .full-height {
-                height: 100vh;
+            body {
+                background-color: #c9dbe9;
+                overflow: hidden;
             }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
+            #clouds {
+                bottom: 0;
+                left: 0;
+                padding-top: 50px;
+                position: fixed;
+                right: 0;
+                top: 0;
+                z-index: -1;
             }
 
-            .position-ref {
+            .cloud {
                 position: relative;
             }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
+            .x1 {
+                -webkit-animation: animateCloud 35s linear infinite;
+                -moz-animation: animateCloud 35s linear infinite;
+                animation: animateCloud 35s linear infinite;
+                -webkit-transform: scale(0.65);
+                -moz-transform: scale(0.65);
+                transform: scale(0.65);
             }
 
-            .content {
+            .x2 {
+                -webkit-animation: animateCloud 20s linear infinite;
+                -moz-animation: animateCloud 20s linear infinite;
+                animation: animateCloud 20s linear infinite;
+                -webkit-transform: scale(0.3);
+                -moz-transform: scale(0.3);
+                transform: scale(0.3);
+            }
+
+            .x3 {
+                -webkit-animation: animateCloud 30s linear infinite;
+                -moz-animation: animateCloud 30s linear infinite;
+                animation: animateCloud 30s linear infinite;
+                -webkit-transform: scale(0.5);
+                -moz-transform: scale(0.5);
+                transform: scale(0.5);
+            }
+
+            .x4 {
+                -webkit-animation: animateCloud 18s linear infinite;
+                -moz-animation: animateCloud 18s linear infinite;
+                animation: animateCloud 18s linear infinite;
+                -webkit-transform: scale(0.4);
+                -moz-transform: scale(0.4);
+                transform: scale(0.4);
+            }
+
+            .x5 {
+                -webkit-animation: animateCloud 25s linear infinite;
+                -moz-animation: animateCloud 25s linear infinite;
+                animation: animateCloud 25s linear infinite;
+                -webkit-transform: scale(0.55);
+                -moz-transform: scale(0.55);
+                transform: scale(0.55);
+            }
+
+            @-webkit-keyframes animateCloud {
+                0% {
+                    margin-left: -1000px;
+                }
+                100% {
+                    margin-left: 100%;
+                }
+            }
+
+            @-moz-keyframes animateCloud {
+                0% {
+                    margin-left: -1000px;
+                }
+                100% {
+                    margin-left: 100%;
+                }
+            }
+
+            @keyframes animateCloud {
+                0% {
+                    margin-left: -1000px;
+                }
+                100% {
+                    margin-left: 100%;
+                }
+            }
+
+            .centerconntol {
+                margin-top: 10%;
+            }
+
+            .centerconn {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                z-index: 2;
                 text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Katabakuwu</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">How to Play</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Public Scoreboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <span class="navbar-text">
+                    &copy; Katabakuwu
+                    </span>
                 </div>
             </div>
+        </nav>
+        <div id="clouds">
+            <!-- Time for multiple clouds to dance around -->
+            <div class="x1">
+                <img src="{{ asset('storage/img/cloud.png') }}" class="cloud" height="300px">
+            </div>
+            <div class="x2">
+                <img src="{{ asset('storage/img/cloud.png') }}" class="cloud" height="300px">
+            </div>
+            <div class="x3">
+                <img src="{{ asset('storage/img/cloud.png') }}" class="cloud" height="300px">
+            </div>
+            <div class="x4">
+                <img src="{{ asset('storage/img/cloud.png') }}" class="cloud" height="300px">
+            </div>
+            <div class="x5">
+                <img src="{{ asset('storage/img/cloud.png') }}" class="cloud" height="300px">
+            </div>
         </div>
+        <div class="centerconntol">
+            <img src="{{ asset('storage/img/logo.jpg') }}" class="centerconn img-fluid" width="200px"/>
+            <h1 class="centerconn m-4" style="color: #0c6e43;">Katabakuwu</h1>
+            <a href="#" class="centerconn btn btn-primary" style="max-width: 200px;">Download Now!</a>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     </body>
 </html>
